19b4415648bcf64d574297b154725bb5a70a414b
PYN <yxp7531540@gmail.com>
2019-06-22 19:25:31 -0500

    done

--- src/components/BandInput.js
+++ src/components/BandInput.js
@@ -1,14 +1,34 @@
 // Add BandInput component
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { addBand } from '../actions/bands'
 
 class BandInput extends Component {
+
+  state = {
+    name: ''
+  }
+
+  handleChange = (event) => {
+    this.setState({name: event.target.value})
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+    this.props.addBand(this.state)
+    this.setState({name: ''})
+  }
+
   render() {
     return(
       <div>
-        Band Input
+        <form onSubmit={this.handleSubmit}>
+          <input type='text' value={this.state.name} onChange={this.handleChange} placeholder='Enter Band Name'/>
+          <input type='submit' value='Add Band'/>
+        </form>
       </div>
     )
   }
 }
 
-export default BandInput
+export default connect(null, { addBand })(BandInput)
--- src/containers/BandsContainer.js
+++ src/containers/BandsContainer.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import BandInput from '../components/BandInput'
 
 class BandsContainer extends Component {
   render() {
     return(
       <div>
-        BandsContainer
+        <BandInput />
+        <ul>
+          {this.props.bands.map(band => <li>{band.name}</li>)}
+        </ul>
       </div>
     )
   }
 }
 
-export default BandsContainer
+export default connect(state => ({bands: state.bands}))(BandsContainer);
